9b3f0b497d1a99c57d7c8a905aea75639b82716e
Alejandro Largo <largoalejo@gmail.com>
2023-01-15 19:40:56 -0500

    first commit

---
+++ variables.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func variables() {
+	var string = "string"
+	fmt.Println(string)
+
+	string2 := "string2"
+	fmt.Println(string2)
+
+	num := 10
+	fmt.Println(num)
+
+	real := 10.12
+	fmt.Println(real)
+
+	bool := true
+	fmt.Println(bool)
+
+}
